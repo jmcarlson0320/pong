40481d725dda5a639ef8444692604113bf6d8758
Joshua Carlson <jmcarlson0320@gmail.com>
2020-12-17 14:31:09 -0800

    added player status

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <tiny-fw.h>
+#include <stdio.h>
 
 #define SCALE_FACTOR 3
 #define WIDTH (1920 / SCALE_FACTOR)
@@ -31,24 +32,63 @@ typedef struct {
     ball b;
 } pong;
 
-void player_update(player *p, App *app);
-void player_render(player *p);
+void player_display_status(player *p, int x, int y)
+{
+    char *status;
+    char buffer[80];
+
+    if (p->dir == UP)
+        status = "UP";
+    else if (p->dir == DOWN)
+        status = "DOWN";
+    else
+        status = "STOP";
+
+    snprintf(buffer, 80, "score: %d\npos: %f, %f\ndir: %s\n", p->score, p->pos.e[X_COOR], p->pos.e[Y_COOR], status);
+    draw_text(buffer, x, y, 0xffff00);
+}
+
+void player_init(player *p, int score)
+{
+    p->score = score;
+    p->pos.e[X_COOR] = 0;
+    p->pos.e[Y_COOR] = 0;
+    p->dir = STOP;
+}
+
+void player_update(player *p, App *app)
+{
+    if (app->keyboard.down[KEY_UP])
+        p->dir = UP;
+    else if (app->keyboard.down[KEY_DOWN])
+        p->dir = DOWN;
+    else
+        p->dir = STOP;
+}
+
+void player_render(player *p)
+{
+    player_display_status(p, 50, 50);
+}
 
 void pong_init(pong *game)
 {
-    return;
+    player_init(&game->p1, 100);
 }
 
 void pong_update(pong *game, App *app)
 {
     if (app->keyboard.pressed[KEY_Q])
         app->running = 0;
+    player_update(&game->p1, app);
 }
 
 void pong_render(pong *game)
 {
+    draw_fill_rect(0, 0, WIDTH - 1, HEIGHT - 1, 0x000000);
     draw_text("pong!!!\n"
               "press 'Q' to quit", 0, 0, 0x00ffff);
+    player_render(&game->p1);
 }
 
 int main(int argc, char *argv[])

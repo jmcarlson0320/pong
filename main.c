8e7ea2f16602ebcf269c7cc18fcb3bb196a10290
Joshua Carlson <jmcarlson0320@gmail.com>
2020-12-17 15:49:21 -0800

    process player input, update player position

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <tiny-fw.h>
 #include <stdio.h>
 
-#define SCALE_FACTOR 3
+#define SCALE_FACTOR 4
 #define WIDTH (1920 / SCALE_FACTOR)
 #define HEIGHT (1080 / SCALE_FACTOR)
-#define PADDLE_SPEED 10
+#define PADDLE_SPEED 100
 #define PADDLE_WIDTH 5
 #define PADDLE_HEIGHT 20
 #define BALL_SPEED 10
@@ -44,7 +44,7 @@ void player_display_status(player *p, int x, int y)
     else
         status = "STOP";
 
-    snprintf(buffer, 80, "score: %d\npos: %f, %f\ndir: %s\n", p->score, p->pos.e[X_COOR], p->pos.e[Y_COOR], status);
+    snprintf(buffer, 80, "Player1:\nscore: %d\npos: %f, %f\ndir: %s\n", p->score, p->pos.e[X_COOR], p->pos.e[Y_COOR], status);
     draw_text(buffer, x, y, 0xffff00);
 }
 
@@ -58,17 +58,37 @@ void player_init(player *p, int score)
 
 void player_update(player *p, App *app)
 {
-    if (app->keyboard.down[KEY_UP])
-        p->dir = UP;
-    else if (app->keyboard.down[KEY_DOWN])
-        p->dir = DOWN;
-    else
-        p->dir = STOP;
+    switch (p->dir) {
+        case UP:
+            p->pos.e[Y_COOR] -= (PADDLE_SPEED * app->time.dt_sec);
+            break;
+        case DOWN:
+            p->pos.e[Y_COOR] += (PADDLE_SPEED * app->time.dt_sec);
+            break;
+        default:
+            break;
+    }
 }
 
 void player_render(player *p)
 {
-    player_display_status(p, 50, 50);
+    int x = p->pos.e[X_COOR];
+    int y = p->pos.e[Y_COOR];
+    draw_rect(x, y, x + PADDLE_WIDTH, y + PADDLE_HEIGHT, 0xffffff);
+    player_display_status(p, 0, 0);
+}
+
+void pong_process_input(pong *game, App *app)
+{
+    if (app->keyboard.pressed[KEY_Q])
+        app->running = 0;
+
+    if (app->keyboard.down[KEY_UP])
+        game->p1.dir = UP;
+    else if (app->keyboard.down[KEY_DOWN])
+        game->p1.dir = DOWN;
+    else
+        game->p1.dir = STOP;
 }
 
 void pong_init(pong *game)
@@ -78,16 +98,13 @@ void pong_init(pong *game)
 
 void pong_update(pong *game, App *app)
 {
-    if (app->keyboard.pressed[KEY_Q])
-        app->running = 0;
+    pong_process_input(game, app);
     player_update(&game->p1, app);
 }
 
 void pong_render(pong *game)
 {
     draw_fill_rect(0, 0, WIDTH - 1, HEIGHT - 1, 0x000000);
-    draw_text("pong!!!\n"
-              "press 'Q' to quit", 0, 0, 0x00ffff);
     player_render(&game->p1);
 }
 
